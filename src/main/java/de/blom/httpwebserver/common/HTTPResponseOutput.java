4164d0967d7a234b00f75b7dfab6a248fb29aca6
Lennart Blom <lennart@blom.de>
2018-12-23 20:41:09 +0100

    Add response output helper class

---
+++ src/main/java/de/blom/httpwebserver/common/HTTPResponseOutput.java
@@ -0,0 +1,20 @@
+package de.blom.httpwebserver.common;
+
+import org.apache.commons.httpclient.HttpStatus;
+
+import java.io.PrintWriter;
+
+public class HTTPResponseOutput {
+
+    public static void writeResponse(HttpStatus httpStatus, PrintWriter out){
+
+    }
+
+    static void write200Response(PrintWriter out) {
+
+    }
+
+    static void write404Response(PrintWriter out) {
+
+    }
+}
---
+++ src/test/java/de/blom/httpwebserver/common/HTTPResponseOutputTest.java
@@ -0,0 +1,22 @@
+package de.blom.httpwebserver.common;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HTTPResponseOutputTest {
+
+    @Test
+    public void expectToOutputCorrect200Status() {
+
+    }
+
+    @Test
+    public void expectToOutputCorrect404Status(){
+
+    }
+
+
+
+
+}
