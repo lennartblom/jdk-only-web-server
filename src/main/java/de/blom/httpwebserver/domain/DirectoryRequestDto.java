9c73ac2e44422ae27c4bab88f89c2f0fffd22fbe
Lennart Blom <registrierungen@lennart-blom.de>
2018-12-25 19:36:20 +0100

    Feature/basic http server (#8)
    * Add dummy webservice directory
    
    * Add not_supported page
    
    * Define basic http server and refine codebase
    
    * Add author information
    
    * Refactor localisation of HTTPMethod enum
    
    * Add junit 4 dependency
    
    * Add hamcrest and mockito
    
    * Define tests for content type and closing connection elements
    
    * Add dependency for HttpStatus enum
    
    * Add response output helper class
    
    * Extract file retrievement into method
    
    * Optimize mocked objection
    
    * Create tests for output
    
    * Extract currentDate mock to before method
    
    * Add usage of HTTPResponseOutput class and support for 501 response
    
    * Add 501 status support and refactor response logic
    
    * Extract content type and length output
    
    * Add test for httpResponse
    
    * Log instead of print console
    
    * Break one test for
    
    * Fix test again
    
    * Add new hexagonal architecture approach
    
    * Add directory information retrievement
    
    * Display of directory information via html
    
    * Refactor and cleanup code
    
    * Add 404 handling
    
    * Refactor var naming
    
    * Add 501 handling and handle null input
    
    * Remove unusedHttpMethod

---
+++ src/main/java/de/blom/httpwebserver/domain/DirectoryRequestDto.java
@@ -0,0 +1,23 @@
+package de.blom.httpwebserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class DirectoryRequestDto {
+    private Boolean found;
+    private List<String> subdirectories;
+    private List<String> files;
+
+    public DirectoryRequestDto(){
+        this.found = false;
+        this.subdirectories = new ArrayList<>();
+        this.files = new ArrayList<>();
+    }
+}
