eb431997a9e00b6f113bc39d0cb8253f20ee31b7
Lennart Blom <registrierungen@lennart-blom.de>
2018-12-30 19:23:11 +0100

    Feature/#12 wall html page (#14)
    * Wall overview and POST call for entry search query
    
    * Create creation page
    
    * Optimize imports and add test case
    
    * Clean up code

--- src/test/java/de/blom/httpwebserver/adapter/inbound/http/commons/HttpRequestTest.java
+++ src/test/java/de/blom/httpwebserver/adapter/inbound/http/commons/HttpRequestTest.java
@@ -45,7 +45,18 @@ public class HttpRequestTest {
             "}\n" +
             "\n";
 
-    private static final String TEST_CHROM_REQUEST = "GET / HTTP/1.1\n" +
+    private static final String TEST_HTTP_REQUEST_JSON_BODY_NO_BLANK_LAST_LINE = "POST /testRoute HTTP/1.1\n" +
+            "Host: localhost\n" +
+            "Content-Type: application/json\n" +
+            "cache-control: no-cache\n" +
+            "Postman-Token: 96621bec-a11a-4c9c-a7ff-6a87c9369e02\n" +
+            "{\n" +
+            "  \"name\": \"1531923956.517\",\n" +
+            "  \"login\": \"asd\",\n" +
+            "  \"password\": \"testpassword\"\n" +
+            "}\n";
+
+    private static final String TEST_CHROME_REQUEST = "GET / HTTP/1.1\n" +
             "Host: localhost:8080\n" +
             "Connection: keep-alive\n" +
             "Dec 28, 2018 6:11:18 PM de.blom.httpwebserver.adapter.inbound.http.HttpAdapter main\n" +
@@ -58,6 +69,22 @@ public class HttpRequestTest {
             "Accept-Language: de,en-US;q=0.9,en;q=0.8\n" +
             "Cookie: PHPSESSID=3d8ujruupf7hf7su1gs2c7jcj6; JSESSIONID=1067490977143665192; grafana_sess=32da410c3a4292d1; redirect_to=%252F; io=kucVFKNOXuMq3nDRAAAa\n" +
             " \n";
+
+    private static final String TEST_JQUERY_AJAX_CALL = "POST /wall_entries HTTP/1.1\n" +
+            "Host: localhost:8080\n" +
+            "Connection: keep-alive\n" +
+            "Content-Length: 25\n" +
+            "Pragma: no-cache\n" +
+            "Cache-Control: no-cache\n" +
+            "Accept: application/json, text/javascript, */*\n" +
+            "Origin: http://localhost:8080\n" +
+            "X-Requested-With: XMLHttpRequest\n" +
+            "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36\n" +
+            "Content-Type: application/x-www-form-urlencoded\n" +
+            "Referer: http://localhost:8080/wall_entry_creation.html\n" +
+            "Accept-Encoding: gzip, deflate, br\n" +
+            "Accept-Language: de,en-US;q=0.9,en;q=0.8\n" +
+            "Cookie: PHPSESSID=3d8ujruupf7hf7su1gs2c7jcj6; JSESSIONID=1067490977143665192; grafana_sess=32da410c3a4292d1; redirect_to=%252F; io=kucVFKNOXuMq3nDRAAAa\n";
 
 
     private BufferedReader bufferedReader;
@@ -134,9 +161,20 @@ public class HttpRequestTest {
     }
 
     @Test
+    public void expectToRetrieveHttpWith_jsonBodyNoBlankLine() throws IOException {
+        this.prepareBufferedReader(TEST_HTTP_REQUEST_JSON_BODY_NO_BLANK_LAST_LINE);
+
+        HttpRequest httpRequest = HttpRequest.parseFrom(this.bufferedReader);
+
+        assertThat(httpRequest.getMethod(), is(HttpMethod.POST));
+        assertThat(httpRequest.getUri(), is("/testRoute"));
+        assertThat(httpRequest.getRawBody(), is("{  \"name\": \"1531923956.517\",  \"login\": \"asd\",  \"password\": \"testpassword\"}"));
+    }
+
+    @Test
     public void expectToSetHeadersFromChromGetRequest() throws IOException {
 
-        this.prepareBufferedReader(TEST_CHROM_REQUEST);
+        this.prepareBufferedReader(TEST_CHROME_REQUEST);
         Map<String, String> expectedHeaders = new HashMap<>();
         expectedHeaders.put("Content-Type", "application/json");
         expectedHeaders.put("Host", "localhost");
@@ -147,6 +185,17 @@ public class HttpRequestTest {
         assertThat(httpRequest.getMethod(), is(HttpMethod.GET));
     }
 
+
+
+    @Test
+    public void expecToParseHttpRequestWithJqueryContent() throws IOException {
+        this.prepareBufferedReader(TEST_JQUERY_AJAX_CALL);
+
+        HttpRequest httpRequest = HttpRequest.parseFrom(this.bufferedReader);
+
+
+        assertThat(httpRequest.getMethod(), is(HttpMethod.POST));
+    }
 
     @Test
     public void expectToIdentifyGetMethod() {
